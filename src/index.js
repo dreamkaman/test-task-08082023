506948a4850dc28cdef8a1ea9c53dd4b9e8ff304
Dreamkaman <riabushko@gmail.com>
2023-08-08 22:53:29 +0300

    First settings

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// import index files block
+import './index.html'; //for 'html-loader'
+import './index.scss'; //for 'mini-css-extract-plugin'
---
+++ webpack.config.js
@@ -0,0 +1,155 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]', //or you can use 'assets/[hash][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'header.html'),
+      location: 'header',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'hero.html'),
+      location: 'hero',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'section-about.html'),
+      location: 'about-us',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'section-contact-us.html'),
+      location: 'contact-us',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'section-our-projects.html'),
+      location: 'our-projects',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'section-our-tasks.html'),
+      location: 'our-tasks',
+      template_filename: 'index.html',
+    }),
+    new HtmlWebpackPartialsPlugin({
+      path: path.resolve(__dirname, 'src/partials', 'footer.html'),
+      location: 'footer',
+      template_filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          'group-css-media-queries-loader',
+          {
+            loader: 'resolve-url-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: devMode
+          ? []
+          : [
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                  },
+                  // optipng.enabled: false will disable optipng
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.9],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  // the webp option will enable WEBP
+                  webp: {
+                    quality: 75,
+                  },
+                },
+              },
+            ],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
